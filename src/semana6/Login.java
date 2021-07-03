c4d35d1d675dbcdd601cde3da4ca2261ce7ebbaf
Jose Enrique Avila <jeavila@unah.edu.hn>
2021-07-03 11:39:54 -0600

    GUI: Login/Registro/VentanaSecreta ActionListener MouseListener

--- src/semana6/Login.java
+++ src/semana6/Login.java
@@ -9,6 +9,8 @@ import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,31 +22,39 @@ import javax.swing.JTextField;
  *
  * @author enrique
  */
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener {
+
     // atributos
     private JButton btnEntrar;
+    private JButton btnRegistrar;
     private JLabel lblUsuario;
     private JLabel lblPassword;
+    private JLabel lblClase;
     private JTextField txtUsuario;
     private JPasswordField txtPassword;
     private JPanel panel;
-    
+    private JPanel panelBoton;
+
     // funcion constructora
     public Login(String titulo) {
         // Inicializando los elementos propios de JFrame (clase de la cual heredamos)
         super(titulo);
         // Inicializacion propia
         btnEntrar = new JButton("Entrar");
+        btnRegistrar = new JButton("Registrar");
         lblUsuario = new JLabel("Usuario:");
         lblPassword = new JLabel("Clave:");
-        txtUsuario = new JTextField(10);
-        txtPassword = new JPasswordField(10);
+        txtUsuario = new JTextField(15);
+        txtPassword = new JPasswordField(15);
         panel = new JPanel(new GridBagLayout());
-        
+        lblClase = new JLabel("Clase: Programacion II");
+        panelBoton = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout(10, 10));
         this.add(panel, BorderLayout.CENTER);
-        
+        this.add(lblClase, BorderLayout.SOUTH);
+
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = 0;
         constraints.gridy = 0;
@@ -58,22 +68,70 @@ public class Login extends JFrame {
         constraints.gridx = 1;
         constraints.gridy = 1;
         panel.add(txtPassword, constraints);
-        constraints.gridx = 1;
+        constraints.gridx = 0;
         constraints.gridy = 2;
-        panel.add(btnEntrar, constraints);
-        
+        constraints.gridwidth = 2;
+        panel.add(panelBoton, constraints);
+
+        panelBoton.add(btnEntrar);
+        panelBoton.add(btnRegistrar);
+
         lblUsuario.setLabelFor(txtUsuario);
         lblPassword.setLabelFor(txtPassword);
+
+        btnEntrar.addActionListener(this);
+        btnRegistrar.addActionListener(this);
         
+        // ActionListener funciona a nivel de componente
+        txtUsuario.addActionListener((ActionEvent e) -> {
+            txtPassword.setFocusable(true);
+            txtPassword.requestFocus();
+        });
 
         //this.pack();
         this.setSize(500, 300);
         this.setVisible(true);
     }
-    
+
+    public boolean autenticar(String usuario, String password) {
+        if (usuario.equals("admin") && password.equals("1234")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void pasarVentanaSecreta() {
+        boolean puedeIngresar = autenticar(txtUsuario.getText(),
+                String.valueOf(txtPassword.getPassword()));
+
+        if (puedeIngresar) {
+            VentanaSecreta secreto = new VentanaSecreta();
+            secreto.mostrar();
+            //this.dispose();
+        }
+    }
+
+    public void pasarVentanaRegistro() {
+        Registro registro = new Registro();
+        registro.mostrar();
+        //this.dispose();
+    }
+
     public static void main(String[] args) {
         Login ventana = new Login("Acceso");
     }
-    
-}
 
+    // El ActionListener funciona a nivel de toda la ventana (concentra la escucha)
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton btn = (JButton) e.getSource();
+
+        if ("Entrar".equals(btn.getText())) {
+            pasarVentanaSecreta();
+        } else if ("Registrar".equals(btn.getText())) {
+            pasarVentanaRegistro();
+        }
+    }
+
+}
